088917e89970be50ceb232ce1dacf6d64047197d
James Mackie <jc.mackie@gmail.com>
2018-09-16 22:32:34 +1000

    Improved display code to now show detected episode and show name. Added tmdb library and tested with hard coded show name. Cleaned up unused code.

--- renderer.js
+++ renderer.js
@@ -3,57 +3,10 @@ const fs = require('fs');
 // Load the shell module from electron so that we can open files
 const {shell} = require('electron');
 
-// open a folder and list its contents
-function readFolder(path) {
-  fs.readdir(path, (err, files) => {
-      'use strict';
-      //if an error is thrown when reading the directory, we throw it. Otherwise we continue
-      if (err) throw  err;
-
-      //Dynamically add <ol> tags to the div
-      document.getElementById('listed-files').innerHTML = `<ol id="display-files"></ol>`;
-
-      // loop through the list of files
-      for (let file of files) {
-        fs.stat(path+file,(err,stats)=>{
-          if(err) throw err;
-          /**
-          * When you double click on a folder or file, we need to obtain the path and name so that we can use it to take action.
-          * The easiest way to obtain the path and name for each file and folder, is to store that information in the element itself, as an ID.
-          * This is possible since we cannot have two files with the same name in a folder.
-          * theID variable below is created by concatenating the path with file name and a / at the end.
-          * As indicated earlier, we must have the / at the end of the path.
-          */
-          let theID = `${path}${file}/`;
-          /**
-          * Check whether the file is a directory or not
-          */
-          if(stats.isDirectory()){
-              /**
-               * Add an ondblclick event to each item.
-               * With folders, call this same function (recursion) to read the contents of the folder.
-               *
-               */
-              document.getElementById('display-files').innerHTML += `<li id=${theID} ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
-          }
-          else {
-            /**
-             * Add an ondblclick event to each item.
-             * If its a file, call the openFile function to open the file with the default app.
-             *
-             */
-            document.getElementById('display-files').innerHTML += `<li id=${theID} ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
-          }
-        });
-      }
-  });
-}
-
-//open the file with the default application
-function openFile(path) {
-    shell.openItem(path);
-}
-
+// Load TMDB library so we can query TMBD!
+// https://github.com/cavestri/themoviedb-javascript-library/
+const tmdb = require('themoviedb-javascript-library');
+let tmdbApiKey = "";
 
 function setupDragSpot(){
   var holder = document.getElementById('drag-file');
@@ -73,23 +26,34 @@ function setupDragSpot(){
   holder.ondrop = (e) => {
       e.preventDefault();
 
-      //Dynamically add <ul> tags to the file-list div
-      document.getElementById('file-list').innerHTML = `<ul id="display-files"></ul>`;
 
       // Our list of file names
-      var file_list = [];
+      var fileList = [];
 
       for (let f of e.dataTransfer.files) {
-          document.getElementById('display-files').innerHTML += `<li><span class="fa fa-file"></span> ${f.name}</li>`;
-          file_list.push(f.name);
+          fileList.push(f.name);
       }
-      parseFilenames(file_list);
 
+      displayFileList(fileList);
       return false;
   };
 }
 
+function displayFileList(fileList){
+  // parse the fileList into a list of tvshows and episodes
+  let parsedShowList = parseFilenames(fileList);
+
+  //Dynamically add <ul> tags to the file-list div
+  document.getElementById('file-list').innerHTML = `<ul id="display-files"></ul>`;
+  for (let p of parsedShowList) {
+      document.getElementById('display-files').innerHTML += `<li><span class="fa fa-file"></span> ${p.filename}<ul><li><span class="fa fa-tv"></span> ${p.showName}</li><li><span class="fa fa-hashtag"> ${p.episode}</li></ul></li>`;
+  }
+
+}
+
 function parseFilenames(file_list){
+  // We will pack the parsed file list into a list of objects
+  var parsedList = [];
   // The regex we use to try and find the episode number details
   var episodeRegex = 'S\\d{1,2}E\\d{1,2}';
   for (let file of file_list) {
@@ -105,6 +69,22 @@ function parseFilenames(file_list){
     // Remove any non alphanumeric characters
     let tvShowWithWhitespace = tvShow.replace(/[\W_]+/g, " ");
 
+    // Combine them into an object
+    let parsedFile = {'filename': file, 'showName': tvShowWithWhitespace, 'episode': episodeMatch[0]}
+
+    parsedList.push(parsedFile);
+
     console.log(`Found tv show ${tvShowWithWhitespace} with episode number ${episodeMatch[0]}`);
+    console.log(`Searching tmdb`);
+    searchTmdb(tvShowWithWhitespace);
   }
+
+  return parsedList;
+}
+
+function searchTmdb(query){
+  query = 'terrace house';
+  let tmdbQuery = {'query': query};
+  tmdb.common.api_key = tmdbApiKey;
+  tmdb.search.getTv(tmdbQuery, function(data){console.log(`Success: ${data}`)},  function(data){console.log(`Error: ${data}`)});
 }
