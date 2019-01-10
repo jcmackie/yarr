61b1ce6fef1e3f5dce94332d5d2b0f6ffbec52f1
James Mackie <jc.mackie@gmail.com>
2019-01-10 23:06:21 +1100

    Implement page fetch with r2, and ran into a weird bug. Worked around it. Next step is jssoup.

--- thetvdb.js
+++ thetvdb.js
@@ -3,8 +3,17 @@ const r2 = require("r2");
 
 function getTvEpisodeMetadata(url) {
   return new Promise((resolve, reject) => {
-    resolve(url)
+    // Some kind of weird bug with R2 and certain web pages:
+    // https://github.com/mikeal/r2/issues/45
+    r2(url).response.then( result => {
+      console.log("we got a result")
+      resolve(result.text())
+    }, fail => {
+      console.error(fail)
+      reject(fail)
+    })
   })
 }
+
 
 exports.getTvEpisodeMetadata = getTvEpisodeMetadata
